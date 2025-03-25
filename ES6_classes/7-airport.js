987a3e4e27fa4f6668cca935d86dfdd19d15c70c
vidad <vidad31@gmail.com>
2025-03-25 14:08:19 +0100

    modif npx

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,12 +1,5 @@
 export default class Airport {
   constructor(name, code) {
-    // Check that name and code are strings
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
     this._name = name;
     this._code = code;
   }
@@ -18,8 +11,13 @@ export default class Airport {
   get code() {
     return this._code;
   }
-// Overrides the toString method to return the airport code
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+
+  // Overrides the toString method to return the airport code
   toString() {
-    return `object [${this._code}]`;
+    return `[object ${this._code}]`;
   }
 }
