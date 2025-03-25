d1d8a1e62cf9a03f172fe9e9646fb5ee7b5e9c20
vidad <vidad31@gmail.com>
2025-03-25 14:45:31 +0100

    modif npx

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -17,7 +18,6 @@ const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
