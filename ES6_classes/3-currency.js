6598971fe853a44cff271f78cc5b338dc8fe23eb
vidad <vidad31@gmail.com>
2025-03-25 12:40:24 +0100

    correction ligne  !==

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,9 +1,9 @@
-export default class Currency{
+export default class Currency {
   constructor(code, name) {
-    if (typeof code != 'string') {
+    if (typeof code !== 'string') {
       throw new TypeError('Code must be a string');
     }
-    if (typeof name != 'string') {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
 
