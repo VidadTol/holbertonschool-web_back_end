3273e977dff08b207231d6e0f9d73e2d3edeb744
vidad <vidad31@gmail.com>
2025-03-31 12:34:06 +0200

    correction npx

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,12 +2,10 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const gradeObj = newGrades.find(
-        (grade) => grade.studentId === student.id
-      );
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
       return {
         ...student,
-        grade: gradeObj ? gradeObj.grade : "N/A",
+        grade: gradeObj ? gradeObj.grade : 'N/A',
       };
     });
 }
