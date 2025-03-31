e05a6c2359e3598490c7cf779c8e2739f620429c
vidad <vidad31@gmail.com>
2025-03-31 11:15:41 +0200

    Refactor createInt8TypedArray to use Int8Array for better performance and clarity

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer);
+  const int8 = new Int8Array(buffer);
 
   if (position >= length) {
     throw new Error('Position outside range');
   }
-  view.setInt8 (position, value);
-  return view;
+  int8 [position] = value;
+  return new DataView(buffer);
 }
