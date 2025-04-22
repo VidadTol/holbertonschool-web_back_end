9de2c0d13aafdb90ba72f255bc96139cc97bf6d8
vidad <vidad31@gmail.com>
2025-04-22 10:38:12 +0200

    add doctring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+This module provides a function to add two floating-point numbers.
+"""
+
+
 def add(a: float, b: float) -> float:
-    """
-    Adds two floating-point numbers.
-
-    Args:
-        a (float): The first number.
-        b (float): The second number.
-
-    Returns:
-        float: The sum of the two numbers.
-    """
+    """Adds two numbers and returns the result."""
     return a + b
