e6d69fd0286d7d129964ab7e342ecf979b38ccb0
vidad <vidad31@gmail.com>
2025-04-22 10:43:49 +0200

    doctring

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""Module to demonstrate the use of type annotations in Python."""
+
 
 def to_str(n: float) -> str:
     """returns the string representation of the float"""
