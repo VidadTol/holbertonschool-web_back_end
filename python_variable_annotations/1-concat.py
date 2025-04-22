36d95d11cc8fcdd46e04b561e6978e9f7dc6fd39
vidad <vidad31@gmail.com>
2025-04-22 10:39:25 +0200

    doctring

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+""" 
+This module provides a function to concatenate two strings."""
+
+
 def concat(str1: str, str2: str) -> str:
     """Concatenate two strings"""
     return str1 + str2
