e189beb4ab71c1467fb1da3cc1fd9c710b643c03
vidad <vidad31@gmail.com>
2025-04-24 10:27:59 +0200

    pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -18,4 +18,3 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
     return sorted(delays)
-    
