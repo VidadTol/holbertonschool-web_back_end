d9533290b72a0f65902bce664b291186674b612e
vidad <vidad31@gmail.com>
2025-04-28 19:24:39 +0200

    refactor: add return type annotation to async_comprehension function

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
-import asyncio
-import random
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Collects 10 random numbers using an async
     comprehension over async_generator.
