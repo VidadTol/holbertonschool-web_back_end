6164a57342fb5b156773b06133ddf3183b223db8
vidad <vidad31@gmail.com>
2025-04-28 18:58:00 +0200

    pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,16 +3,19 @@
 import asyncio
 import random
 
+
 async def async_generator():
-  """ 
-  Yields a random number between 0 and 10 every second for 10 seconds.
-  """
-  for i in range(10):
-    await asyncio.sleep(1)
-    yield random.uniform(0, 10)
+    """
+    Yields a random number between 0 and 10 every second for 10 seconds.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
 
 async def async_comprehension():
-  """
-  Collects 10 random numbers using an async comprehension over async_generator.
-  """
-  return [i async for i in async_generator()]
+    """
+    Collects 10 random numbers using an async
+    comprehension over async_generator.
+    """
+    return [i async for i in async_generator()]
