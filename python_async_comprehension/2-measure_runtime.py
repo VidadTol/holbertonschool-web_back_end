bb07e20131fe085faf885771fbdc8cc241c558df
vidad <vidad31@gmail.com>
2025-04-29 10:37:38 +0200

    pycodestyle

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -10,17 +10,16 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-  """
-  Measures the total runtime of async_comprehension
-  and returns it.
-  """
-
-  start_time = time.time()
-  await asyncio.gather(
-    async_comprehension(),
-    async_comprehension(),
-    async_comprehension(),
-    async_comprehension()
-  )
-  end_time = time.time()
-  return end_time - start_time
+    """
+    Measures the total runtime of async_comprehension
+    and returns it.
+    """
+    start_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.time()
+    return end_time - start_time
