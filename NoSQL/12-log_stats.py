5e06ac1919438b610599cf8c891d55f20e022afc
vidad <vidad31@gmail.com>
2025-05-02 10:03:04 +0200

    modif

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,24 +4,25 @@ from pymongo import MongoClient
 
 
 def log_stats():
-  """provides some stats about Nginx logs stored in MongoDB"""
-  client = MongoClient('localhost', 27017)
-  db = client.logs
-  collection = db.nginx
-  
-  total_logs = collection.count_documents({})
-  print(f"{total_logs} logs")
+    """provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('localhost', 27017)
+    db = client.logs
+    collection = db.nginx
 
- 
-  methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-  print("Methods:")
+    logs_qty = collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    status_check = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+        })
 
-  for method in methods:
-    count = collection.count_documents({"method": method})
-    print(f"\tmethod {method}: {count}")
+    print(f"{logs_qty} logs")
+    print("Methods:")
+    for method in methods:
+        method_qty = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_qty}")
+    print(f"{status_check} status check")
 
-  status_check = collection.count_documents({"method": "GET", "path": "/status"})
-  print(f"{status_check} status check")
 
-  if __name__ == "__main__":
+if __name__ == "__main__":
     log_stats()
