20fc991b1cc13d231478f1eca5ab822b79b59a21
vidad <vidad31@gmail.com>
2025-05-19 10:52:09 +0200

    fix

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -9,7 +9,7 @@ function countStudents(path) {
             }
 
             const lines = data.trim().split('\n').filter(line => line.trim() !== '');
-            const students = lines.slice(1); // Exclure l'en-tête
+            const students = lines.slice(1); 
 
             console.log(`Number of students: ${students.length}`);
 
@@ -17,8 +17,8 @@ function countStudents(path) {
 
             students.forEach(line => {
                 const studentData = line.split(',');
-                const name = studentData[0]; // Prénom
-                const field = studentData[studentData.length - 1]; // Domaine
+                const name = studentData[0]; 
+                const field = studentData[studentData.length - 1]; 
                 
                 if (!fields[field]) {
                     fields[field] = [];
@@ -30,7 +30,7 @@ function countStudents(path) {
                 console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
             });
 
-            resolve();
+            resolve({total:students.length, fields });
         });
     });
 }
