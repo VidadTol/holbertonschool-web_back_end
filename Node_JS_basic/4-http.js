dddd7cc0679b6c592ce681893aee66353780480e
vidad <vidad31@gmail.com>
2025-05-20 12:02:57 +0200

    fix

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,12 +1,14 @@
 
 const http = require("http");
 
-const app = http.createServer((_, res) => {
+const port = 1245;
+
+const app = http.createServer((req, res) => {
   res.writeHead(200);
   res.end("Hello Holberton School!");
 })
 
-app.listen(1245, () => {
+app.listen(port, () => {
   console.log("Server is listening on port 1245");
 });
 
