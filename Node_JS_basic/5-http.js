4a84ba0b9c4a6c01c33747dfb6e18907b9fa72bc
vidad <vidad31@gmail.com>
2025-05-20 10:01:36 +0200

    feat: implement HTTP server to serve student data

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const countStudents = require("./3-read_file_async");
+
+const databaseFile = process.argv[2];
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    return res.end("Hello Holberton School!");
+  }
+
+  if (req.url === "/students") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+
+    let reponseText = "This is the list of our students\n";
+
+    try {
+      const studentData = await countStudents(databaseFile); 
+      reponseText += studentData;
+    } catch (error) {
+      res.write(error.message);
+    }
+    return res.end(reponseText);
+  }
+
+  res.writeHead(404);
+  res.end("Page not found");
+});
+
+app.listen(1245, () => {
+  console.log("Server is running on port 1245");
+});
+
+module.exports = app;
